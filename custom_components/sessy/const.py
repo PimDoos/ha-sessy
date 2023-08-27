405010074092e683953d8598128acdd4ca41217a
PimDoos <17709721+PimDoos@users.noreply.github.com>
2023-08-27 19:27:14 +0200

    Add option flow for scan_interval

--- custom_components/sessy/const.py
+++ custom_components/sessy/const.py
@@ -2,8 +2,15 @@ from datetime import timedelta
 from sessypy.api import SessyApiCommand
 
 DOMAIN = "sessy"
+
+# Default scan interval for all entities except power related
 DEFAULT_SCAN_INTERVAL = timedelta(seconds=30)
-SCAN_INTERVAL_POWER = timedelta(seconds=10)
+
+# Default scan interval for power related entities, overridden by scan_interval option config flow
+DEFAULT_SCAN_INTERVAL_POWER = timedelta(seconds=5)
+
+# Static scan intervals
+SCAN_INTERVAL_POWER = timedelta(seconds=5)
 SCAN_INTERVAL_OTA = timedelta(seconds=15)
 SCAN_INTERVAL_OTA_CHECK = timedelta(hours=6)
 
