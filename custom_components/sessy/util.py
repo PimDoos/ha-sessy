a3caac7baad48c0e6da1842441540377fe374ab4
PimDoos <17709721+PimDoos@users.noreply.github.com>
2025-09-08 21:41:49 +0200

    Format with ruff #145

--- custom_components/sessy/util.py
+++ custom_components/sessy/util.py
@@ -13,55 +13,67 @@ from typing import Callable, Optional
 from .const import DOMAIN
 
 import logging
-_LOGGER = logging.getLogger(__name__)
 
+_LOGGER = logging.getLogger(__name__)
 
 
 # Transform functions
 
+
 def backend_status_string(status_string: str, prefix: str = "") -> str:
     return status_string.removeprefix(prefix).lower()
+
 
 def status_string_p1(status_string: str) -> str:
     return backend_status_string(status_string, "P1_")
 
+
 def status_string_system_state(status_string: str) -> str:
     return backend_status_string(status_string, "SYSTEM_STATE_")
+
 
 def status_string_power_strategy(status_string: str) -> str:
     return backend_status_string(status_string, "POWER_STRATEGY_")
 
+
 def divide_by_thousand(input: int) -> float:
     return input / 1000
+
 
 def divide_by_hundred_thousand(input: int) -> float:
     return input / 100000
 
+
 def only_negative_as_positive(input: int) -> int:
     return min(input, 0) * -1
+
 
 def only_positive(input: int) -> int:
     return max(input, 0)
 
+
 def time_from_string(input: str) -> time:
     return datetime.strptime(input, "%H:%M").time()
+
 
 def start_time_from_string(input: str) -> time:
     return time_from_string(input.split("-")[0])
 
+
 def stop_time_from_string(input: str) -> time:
     return time_from_string(input.split("-")[1])
+
 
 def transform_on_list(transform_list: list, transform_function: Callable) -> list:
     transformed = []
     for i in transform_list:
-        transformed.append(
-            transform_function(i)
-        )
+        transformed.append(transform_function(i))
     return transformed
 
 
-def enum_to_options_list(options: Enum, transform_function: Optional[Callable] = None) -> list[str]:
+def enum_to_options_list(
+    options: Enum, transform_function: Optional[Callable] = None
+) -> list[str]:
     output = list()
     for option in options:
         value = option.value
@@ -73,13 +85,15 @@ def enum_to_options_list(options: Enum, transform_function: Optional[Callable] =
 
 
 def unit_interval_to_percentage(input: float) -> float:
-    return round(input * 100,1)
+    return round(input * 100, 1)
+
 
 # End transform functions
 
 
-async def generate_device_info(hass: HomeAssistant, config_entry: ConfigEntry, device: SessyDevice) -> DeviceInfo:
-    
+async def generate_device_info(
+    hass: HomeAssistant, config_entry: ConfigEntry, device: SessyDevice
+) -> DeviceInfo:
     model = "Sessy Device"
     if isinstance(device, SessyBattery):
         model = "Sessy Battery"
@@ -99,6 +113,7 @@ async def generate_device_info(hass: HomeAssistant, config_entry: ConfigEntry, d
     )
 
     return device_info
+
 
 def get_nested_key(data, key):
     if data is None:
